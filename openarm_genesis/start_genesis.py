e1ba6b7fc122375754e910f04f351142a4a2ffca
Thomason Zhou <reazonresearch@gmail.com>
2025-02-19 11:36:56 +0900

    Add MJCF and URDF support in genesis

--- openarm_genesis/start_genesis.py
+++ openarm_genesis/start_genesis.py
@@ -1,15 +1,17 @@
 import os
-import numpy as np
+import torch
 import genesis as gs
 import pathlib
+import math
 
 os.environ["PYOPENGL_PLATFORM"] = "glx"
 
-KP = [85.0, 50.0, 30.0, 30.0, 27.5, 35.0, 35.0, 100.0]
-KV = [18.5, 12.0, 8.0, 10.0, 3.0, 3.0, 3.0, 20.5]
-JOINT_NAMES = ["rev1", "rev2", "rev3", "rev4", "rev5", "rev6", "rev7", "slider_left"]
-FORCE_LOWER = [-87, -87, -87, -87, -12, -12, -12, -12]
-FORCE_UPPER = [87, 87, 87, 87, 12, 12, 12, 12]
+KP = [85.0, 50.0, 30.0, 30.0, 27.5, 35.0, 35.0, 100.0, 100.0]
+KV = [18.5, 12.0, 8.0, 10.0, 3.0, 3.0, 3.0, 20.5, 20.5]
+# mimic joints/tendons are not supported by genesis
+JOINT_NAMES = ["rev1", "rev2", "rev3", "rev4", "rev5", "rev6", "rev7", "slider_left", "slider_right"]
+FORCE_LOWER = [-87, -87, -87, -87, -12, -12, -12, -12, -12]
+FORCE_UPPER = [87, 87, 87, 87, 12, 12, 12, 12, 12]
 ZERO_POS = [0.0] * len(JOINT_NAMES)
 assert len(KP) == len(KV) == len(JOINT_NAMES) == len(FORCE_LOWER) == len(FORCE_UPPER), (
     f"All constants must be length {len(JOINT_NAMES)}"
@@ -20,28 +22,42 @@ USE_GPU = False
 
 def main() -> None:
     print("Setting up genesis")
-    gs.init(backend=gs.gpu if USE_GPU else gs.cpu, logging_level="debug")
+    gs.init(backend=gs.gpu if USE_GPU else gs.cpu, 
+            # logging_level="debug",
+            )
     scene: gs.Scene = gs.Scene(
         sim_options=gs.options.SimOptions(
             substeps=10,
             gravity=(0, 0, -9.81),
         ),
+        show_viewer = gs.platform == "Linux"
     )
 
     scene.add_entity(gs.morphs.Plane())
-    assets_path = str(pathlib.Path(__file__).resolve().parent.parent / "assets/openarm_grip.urdf")
+    assets_path = pathlib.Path(__file__).resolve().parent.parent / "assets"
+    urdf_str = str(assets_path/"openarm_grip.urdf")
+    mjcf_str = str(assets_path/"openarm_grip_mjcf.xml")
 
     POS_ORIGIN = (0, 0, 0)  # m
     BASE_EULER = (0, 0, 0)  # degrees
     HORIZONTAL_EULER = (0, 90, 90)  # degrees
 
     openarm: gs.engine.entities.rigid_entity.rigid_entity.RigidEntity = scene.add_entity(
-        gs.morphs.URDF(
-            file=assets_path,
+        # gs.morphs.URDF(
+        #     file=urdf_str,
+        #     pos=POS_ORIGIN,
+        #     euler=BASE_EULER,
+        #     fixed=True,
+        #     collision=True
+        # ),
+        gs.morphs.MJCF(
+            file=mjcf_str,
             pos=POS_ORIGIN,
             euler=BASE_EULER,
-            fixed=True,
+            collision=True,
+            convexify=False,
         ),
+        vis_mode="visual"
     )
     scene.build()
 
@@ -54,22 +70,48 @@ def main() -> None:
     openarm.control_dofs_position(ZERO_POS, dofs_idx)
 
     scene.step()
-    gs.tools.run_in_another_thread(fn=run_sim, args=(scene, openarm, dofs_idx))
-    scene.viewer.start()
+
+    if gs.platform == "Linux":
+        run_sim(scene=scene, openarm=openarm, dofs_idx=dofs_idx)
+    else:
+        scene.viewer.start()
+        gs.tools.run_in_another_thread(fn=run_sim, args=(scene, openarm, dofs_idx))
 
 
 def run_sim(scene: gs.Scene, openarm, dofs_idx):
+    COMMAND_STEPS = 100
+    increment = COMMAND_STEPS * 0.01 # 1 %
+    curr_step = 0
+    LOWER_LIMIT, UPPER_LIMIT = openarm.get_dofs_limit()
+    moving_towards = LOWER_LIMIT
+    EXAMPLE_INDEX = 0
+    GRIPPER_INDEX_LEFT = 7
+    GRIPPER_INDEX_RIGHT = 8
+
+    command_pos = torch.zeros_like(openarm.get_dofs_position())
     while True:
         positions = openarm.get_dofs_position()
         velocities = openarm.get_dofs_velocity()
         forces = openarm.get_dofs_force()
-
         control_forces = openarm.get_dofs_control_force()
 
-        # print(f"Positions: {positions}")
+        print(f"Positions: {positions}")
         # print(f"Velocities: {velocities}")
         # print(f"Forces: {forces}")
         # print(f"Control Forces: {control_forces}")
+
+        curr_step += increment
+
+        command_pos[EXAMPLE_INDEX] = (curr_step / COMMAND_STEPS) * (LOWER_LIMIT[EXAMPLE_INDEX] if curr_step < 0 else UPPER_LIMIT[EXAMPLE_INDEX]) 
+        command_pos[GRIPPER_INDEX_LEFT] = abs(curr_step / COMMAND_STEPS) * LOWER_LIMIT[GRIPPER_INDEX_LEFT] # negative range of motion only
+        command_pos[GRIPPER_INDEX_RIGHT] = abs(curr_step / COMMAND_STEPS) * LOWER_LIMIT[GRIPPER_INDEX_RIGHT] # negative range of motion only
+
+        if not torch.all((LOWER_LIMIT <= command_pos) & (command_pos <= UPPER_LIMIT)):
+            increment *= -1.0
+            print(f"Control positions out of bounds, ignoring")
+        else:
+            print(f"Sent command {command_pos}")
+            openarm.control_dofs_position(command_pos)
 
         scene.step()
     scene.viewer.stop()
